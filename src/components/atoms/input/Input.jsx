7102b42e04c4a7c7f42569a6087462a83c6b0b11
Juan Jose Pelaez <juanhernandez.code@gmail.com>
2022-01-06 18:06:38 -0500

    add input component

---
+++ src/components/atoms/input/Input.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './input.scss';
+
+const Input = (props) => {
+  const { name, id, onClick, children, className, appearance, type, style, title, } = props;
+  return (
+    <input
+      id={id || null}
+      type={ 'text' || type}
+      className={`input__container ${className}`}
+      style={style}
+      title={title}
+      placeholder={title || "input placeholder"}
+    />
+
+  );
+};
+
+export default Input;
